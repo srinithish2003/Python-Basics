55e8d10dc37b3c074e5c227778195d4ef4f78106
Sri Nithish Ram Mohan <102612548+srinithish2003@users.noreply.github.com>
2026-08-29 15:24:01 +0530

    Delete Python udemylearning/Day 1/code4.py

--- Python udemylearning/Day 1/code4.py
+++
@@ -1,9 +0,0 @@
-#area of a sphere 
-radius=int(input("\nEnter the radius:"))
-pi=3.14 #fixed value
-area=4*pi*radius*radius
-
-print(f"\n\nArea of the sphere {area}")
-#output
-# Enter the radius:22
-# Area of the sphere 6079.04
