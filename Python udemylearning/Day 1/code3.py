7c7b30b92bb9d09b8281cd8c8ceee3624b1928a0
Sri Nithish Ram Mohan <102612548+srinithish2003@users.noreply.github.com>
2026-08-29 15:12:55 +0530

    Add files via upload

---
+++ Python udemylearning/Day 1/code3.py
@@ -0,0 +1,9 @@
+print("Welcome to the new code!!")
+city=input("enter your city: ")
+name=input("enter your name:")
+print(f"Hello your name is {name} and you live in {city}!")
+# output
+# Welcome to the new code!!
+# enter your city: vellore 
+# enter your name:nithish 
+# Hello your name is nithish  and you live in vellore !
