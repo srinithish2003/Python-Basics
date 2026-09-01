8ecea17d659c8be8b66efed0e3a9c19f8a916116
Sri Nithish Ram Mohan <102612548+srinithish2003@users.noreply.github.com>
2026-09-01 19:41:02 +0530

    Day 4 learning

---
+++ Day 4/loops.py
@@ -0,0 +1,7 @@
+fruits=["apple","banana","cherry","mango","avocado"]
+# fruit is the variable and fruits is the iterable. so what happens here is every item of the
+# list is visited so that it can be printed or modified according to teh problem statement
+for fruit in fruits:
+    # print(fruit)
+    print(fruit + " Pie") # this line appends the string " Pie" to every list item
+
